124a96e5bd55a6ddb977ba813885e1c0e65d5f39
anshulrdamco <47388377+anshulrdamco@users.noreply.github.com>
2019-11-28 15:35:34 +0530

    commit-3

---
+++ web-server/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+        res.send('Hello');
+});
+
+app.get('/help', (req, res) => {
+    res.send('Help page');
+});
+
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+
+app.get('/weather', (req, res) => {
+    res.send('Weather page');
+});
+
+app.listen(3000, () => {
+    
+    console.log("Listening on Port 3000");
+})  
+
