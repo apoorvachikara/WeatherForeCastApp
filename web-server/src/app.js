6bc261af985f215092ffc6df9abd35c62e524489
anshulrdamco <47388377+anshulrdamco@users.noreply.github.com>
2019-12-09 11:20:58 +0530

    commit-4

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const app = express();
-var handlebars  = require('express-handlebars');
+var handlebars  = require('hbs');
 const path = require('path');
+
 const publicDirectory = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 app.use(express.static(publicDirectory));
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', viewsPath);
 app.set('view engine', 'hbs');
+handlebars.registerPartials(partialPath);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        'title': 'Weather App',
+        'name': 'Apoorva Chikara'
+    });
 })
 app.get('/help', (req, res) => {
-    // res.send('help.html');
+    res.render('help', {
+        title: 'Help',
+        name: 'Apoorva Chikara',
+        message: 'I am on Help Page'
+    });
 });
 
 
 app.get('/about', (req, res) => {
-    // res.send('about');
+    res.render('about', {
+        'title': 'About',
+        'name': 'Apoorva Chikara'
+    });
 });
 
 
